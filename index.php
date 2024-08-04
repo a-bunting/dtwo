3902918a7fa583465a68c70888942ad0ad7f4848
Alex Bunting <alex.bunting@gmail.com>
2024-08-04 22:47:13 +0100

    one

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alex Buntings D2 application</title>
+
+    <link rel="stylesheet" href="./css/css.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="./javascript/js.js"></script>
+</head>
+
+<body>
+
+    <div class="structure__menu">
+        <div class="structure__name">
+            <div class="structure__name--name">Alex</div>
+            <div class="structure__name--name">Bunting</div>
+        </div>
+
+        <div class="structure__line"></div>
+
+        <div class="structure__dtwo">Application for the Junior Website Developer position</div>
+
+        <div class="structure__line"></div>
+
+        <div class="structure__buttons">
+            <div class="structure__button">Alex</div>
+            <div class="structure__button">UI/UX</div>
+            <div class="structure__button">Coding</div>
+            <div class="structure__button">Soft Skills</div>
+            <div class="structure__button">Databases</div>
+            <div class="structure__button">Other Reasons</div>
+        </div>
+
+    </div>
+
+    <div id="load" class="structure__load">
+
+    </div>
+
+</body>
+
+</html>
